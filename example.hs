2ec32c0e07a250f19aed96d0c9b3bc35cdda25cf
Roel van Dijk <vandijk.roel@gmail.com>
2010-04-07 13:58:10 +0200

    Updated copyright, data files, example and dependencies
    Ignore-this: a459c9277a5ad394879f0b4ad9f77932
    
    darcs-hash:20100407115810-b004c-bc8cf35055a6d26d9ad1833ed239442ef4a2a776.gz

--- example.hs
+++ example.hs
@@ -1,28 +1,34 @@
 module Main where
 
 import System.USB.IDDB
-import System.USB.IDDB.LinuxUsbIdRepo (staticDb)
+import qualified System.USB.IDDB.UsbDotOrg      as U (staticDb)
+import qualified System.USB.IDDB.LinuxUsbIdRepo as L (staticDb)
 import Text.Printf (printf)
 
 
 main :: IO ()
-main = do -- Load a snapshot from the linux-usb.sourceforget.net database.
-          db <- staticDb
+main = do putStrLn "Data from usb.org:"
+          example =<< U.staticDb
+          putStrLn ""
+          putStrLn "Data from linux-usb.org:"
+          example =<< L.staticDb
 
-          -- Print the name of vendor 0x1d6b
-          putStrLn $ maybe "unknown VID!" id
-                   $ vendorName db 0x1d6b
+example :: IDDB -> IO ()
+example db =
+  do -- Print the name of vendor 0x1d6b
+     putStrLn $ maybe "unknown VID!" id
+              $ vendorName db 0x1d6b
 
-          -- Print the ID of "Linux Foundation"
-          putStrLn $ maybe "unknown vendor name!" (printf "0x%04x")
-                   $ vendorId db "Linux Foundation"
+     -- Print the ID of "Linux Foundation"
+     putStrLn $ maybe "unknown vendor name!" (printf "0x%04x")
+              $ vendorId db "Linux Foundation"
 
-          -- Print the name of the product with ID 0x0101 from the
-          -- vendor with ID 0x1d6b.
-          putStrLn $ maybe "unknown PID!" id
-                   $ productName db 0x1d6b 0x0101
+     -- Print the name of the product with ID 0x0101 from the
+     -- vendor with ID 0x1d6b.
+     putStrLn $ maybe "unknown PID!" id
+              $ productName db 0x1d6b 0x0101
 
-          -- Print the ID of the product with the name "Audio Gadget"
-          -- from the vendor with ID 0x1d6b.
-          putStrLn $ maybe "unknown product name!" (printf "0x%04x")
-                   $ productId db 0x1d6b "Audio Gadget"
+     -- Print the ID of the product with the name "Audio Gadget"
+     -- from the vendor with ID 0x1d6b.
+     putStrLn $ maybe "unknown product name!" (printf "0x%04x")
+              $ productId db 0x1d6b "Audio Gadget"
